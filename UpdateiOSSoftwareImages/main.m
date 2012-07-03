16cab268553afcd3ee325593c6798e1015183bc9
Sinoru Dev <sinoru.dev@me.com>
2012-07-03 20:33:45 +0900

    Initial Commit

---
+++ UpdateiOSSoftwareImages/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UpdateiOSSoftwareImages
+//
+//  Created by Sinoru on 12. 7. 3..
+//  Copyright (c) 2012년 Sinoru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
